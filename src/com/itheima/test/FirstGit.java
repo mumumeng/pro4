8c7b025f5bf9049f259360cd86072a330931093f
limeng <limeng@itcast.com>
2018-12-16 21:23:35 +0800

    提交本地代码到git中

---
+++ src/com/itheima/test/FirstGit.java
@@ -0,0 +1,11 @@
+package com.itheima.test;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
